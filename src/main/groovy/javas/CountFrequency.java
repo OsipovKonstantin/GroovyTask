5048da76f0ab1f1e4a0e63923436d53b6ca450f4
Константин <zed726804@yandex.ru>
2024-05-30 21:54:41 +0300

    Groovy Task for RT-Inform company. Input: list or array of integers. Output: map of frequency of each element. Program written in both Java and Groovy.

---
+++ src/main/groovy/javas/CountFrequency.java
@@ -0,0 +1,14 @@
+package java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountFrequency {
+    public static Map<Integer,Integer> count(int[] array) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(int element: array) {
+            frequencyMap.put(element, frequencyMap.getOrDefault(element, 0) +1);
+        }
+        return frequencyMap;
+    }
+}
